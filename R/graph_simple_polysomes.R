f2ffa8c1ca886e93d7bd2d2c9103b75576fee608
John Favate <jsfavate@gmail.com>
2020-10-14 11:30:06 -0400

    Added graph_simple_polysomes.R function.

---
+++ R/graph_simple_polysomes.R
@@ -0,0 +1,23 @@
+#' Graph a single polysome profile
+#'
+#' This function graphs data from a single sample with no fraction markings. It takes a data frame created by read_polysomes
+#' and the minimum distance from the top of the gradient at which you want to start graphing from.
+#'
+#' @param x data frame created by read_polysomes function.
+#' @param min_position minimum position from top of gradient at which to start graphing from
+#' @export
+graph_simple_polysomes <- function(x, min_position){
+  # filter the df to a min position
+  df <- x %>%
+    dplyr::filter(position_mm >= min_position)
+
+  df %>%
+    ggplot2::ggplot(., ggplot2::aes(position_mm, absorbance))+
+    ggplot2::geom_line(size = .6)+
+    ggplot2::theme_classic()+
+    ggplot2::theme(text = ggplot2::element_text(size = 14))+
+    ggplot2::labs(x = "Position from top of gradient (mm)",
+                  y = "Absorbance")
+
+  return(p)
+}
